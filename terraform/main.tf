227c4eb055fad4b619ac2d91c1f12632f5ce0191
Hallef <hallef28@gmail.com>
2022-06-15 14:31:22 -0300

    started

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,26 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 3.27"
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
     }
   }
   required_version = ">= 0.14.9"
 }
 
 provider "aws" {
-    
-    profile = "default"
-    region = "us-east-1"
+
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+
+  ami           = "ami-08d4ac5b634553e16"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Example"
+  }
+
 }
